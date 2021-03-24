7a5145066e47fa7e97bc08332a7981a8124314a9
unknown <anilemreciftci@gmail.com>
2021-03-24 19:30:25 +0300

    fe framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./scss/style.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+);
+
+// Containers
+const TheLayout = React.lazy(() => import("./containers/TheLayout"));
+
+// Pages
+const Login = React.lazy(() => import("./views/pages/login/Login"));
+const Register = React.lazy(() => import("./views/pages/register/Register"));
+const Page404 = React.lazy(() => import("./views/pages/page404/Page404"));
+const Page500 = React.lazy(() => import("./views/pages/page500/Page500"));
+
+class App extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
 }
 
 export default App;
