3dd9f55b0a9ce8270810799ef9db83782213ebe1
unknown <anilemreciftci@gmail.com>
2021-03-24 19:18:04 +0300

    change server

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/v1/locations', locationRoutes);
 
 //serve static assets in prod
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
